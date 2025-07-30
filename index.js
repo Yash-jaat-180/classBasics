99c27e1cacc4ae1cfa879fb37740c7d2e0d9e657
Yash <yashsaharan180@gmail.com>
2025-07-30 11:48:16 +0530

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 
 const app = express();
-const PORT = 80;
+const PORT = 8000;
 app.use(express.static('public'));
 app.use((req, res, next, err) => {
     console.log("Some error occured"); // if any error occur in processing req then control will come here.
